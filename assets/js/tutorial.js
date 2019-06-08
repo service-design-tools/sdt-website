6f7082d8dd2433cc956d78b55b20a5a097f62198
Mitch90 <Mitch90@users.noreply.github.com>
2019-06-08 17:53:57 +0200

    create tutorial layout

---
+++ assets/js/tutorial.js
@@ -0,0 +1,29 @@
+(function () {
+    const $timelineSteps = document.querySelectorAll('.timeline__step');
+    const timelineStepsArray = [...$timelineSteps];
+    enterView({
+        selector: '.step',
+        enter: function(el) {
+            const stepIndex = el.getAttribute('data-step');
+            $timelineSteps.forEach(step => {
+                step.classList.remove('active');
+                step.classList.remove('inactive-backward');
+                step.classList.remove('active-backward');
+            });
+            timelineStepsArray[stepIndex].classList.add('active');
+        },
+        exit: function(el) {
+            const stepIndex = parseInt(el.getAttribute('data-step'));
+            $timelineSteps.forEach(step => {
+                step.classList.remove('active');
+                step.classList.remove('inactive-backward');
+                step.classList.remove('active-backward');
+            });
+            timelineStepsArray[stepIndex].classList.add('inactive-backward');
+            if (stepIndex > 0) {
+                timelineStepsArray[stepIndex - 1].classList.add('active-backward');
+            }
+        },
+        offset: 0.5
+    });
+})();
